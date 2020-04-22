2117490802d2912df2b403838769003e3d5a5250
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-04-22 18:24:03 +0400

    bool constructor

--- 01_types.js
+++ 01_types.js
@@ -1,9 +1,9 @@
+'use strict';
 
-let num;
-console.log(num);
-
-var num2;
-console.log(num2);
+// let num;
+// console.log(num);
+// var num2;
+// console.log(num2);
 
 console.log(typeof Math)
 console.log(undefined == null)
@@ -12,6 +12,12 @@ console.log(typeof NaN)
 console.log(typeof Infinity)
 console.log(typeof 1/0)
 console.log(1 / 0);
+
+//how recognize to types Boolean() class
+console.log('>>>>>>>>>>>>>>>>>>>>>>')
+console.log('string to Boolean constructor:', Boolean('JavaScript'));
+console.log('empty str to Bool constructor:', Boolean(''));
+
 
 
 
