e194ff89e447ac9538ca05b2785121bb06575478
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-04-25 21:19:47 +0400

    exploring types

--- 01_types.js
+++ 01_types.js
@@ -5,10 +5,14 @@
 // var num2;
 // console.log(num2);
 
+//typeof operator demonstration of all types
 console.log(typeof Math)
+console.log(typeof false)
 console.log(undefined == null)
 console.log(undefined == NaN)
 console.log(typeof NaN)
+console.log(typeof 'JavaScript')
+console.log(typeof 8848)
 console.log(typeof Infinity)
 console.log(typeof 1/0)
 console.log(1 / 0);
@@ -17,8 +21,8 @@ console.log(1 / 0);
 console.log('>>>>>>>>>>>>>>>>>>>>>>')
 console.log('string to Boolean constructor:', Boolean('JavaScript'));
 console.log('empty str to Bool constructor:', Boolean(''));
-
-
+console.log('empty func to Bool constructor', Boolean(function(){} ))
+console.log('empty object to Bool constructor', Boolean({}))
 
 
 
