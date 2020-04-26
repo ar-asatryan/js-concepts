004a92ee2715245177f34508edf9d1c06e62c7ba
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-04-26 23:49:19 +0400

    return function and invoke

--- 05_clojures.js
+++ 05_clojures.js
@@ -12,3 +12,16 @@ let incrCount = counter();
 incrCount();
 incrCount();
 incrCount();
+
+
+console.log('==========');
+function operationFirst(num1) {
+    var summ = 48;
+    function operationSecond(num2) {
+        return num1 + num2;
+    }
+    return operationSecond;
+}
+
+let plusValue = operationFirst(12);
+plusValue(3);
