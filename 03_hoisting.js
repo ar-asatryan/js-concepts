7f8a6bbb7b9294fe93038ab1d1e69486cf4f6b12
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-04-28 03:05:52 +0400

    hoisting practising

---
+++ 03_hoisting.js
@@ -0,0 +1,15 @@
+//hoisting practice
+//'use strict'
+
+var package = 14;
+console.log(package);
+
+//var i;
+console.log(`value of i: ${i}`)
+var i = 15;
+
+
+//let num;
+console.log(`value of num: ${num}`)
+let num = 'message';
+
