b2a4cc3961a100fb0c067768db5c91d004bf8204
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-05-05 06:29:15 +0400

    getMax and getMin functions

--- arrays_tasks.js
+++ arrays_tasks.js
@@ -1,7 +1,7 @@
 'use strict';
-//Given an array, find max value
 
-let arr = [23, 14, 53, 4, 0, 56, 7, 0, 19, 3];
+//Given an array, find max value
+let arr = [23, 14, 53, 4, 0, 56, 7, -49, 0, 19, 3];
 
 let max = arr[0];
 for(let i = 0; i <= arr.length; i++) {
@@ -12,5 +12,34 @@ for(let i = 0; i <= arr.length; i++) {
 
 }
 
-console.log('max is:', max)
+//console.log(arr)
+//console.log('max is:', max)
 
+//find the max value with function declaration
+function getMax(arr) {
+    let max = arr[0];
+
+    for(let i = 0; i <= arr.length; i++) {
+        if(arr[i] >= max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log('max value is:', getMax(arr));
+
+
+//find the min value from the given array
+
+const getMin = function(arr) {
+  let min = arr[0];
+  for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] <= min) {
+          min = arr[i];
+      }
+  }
+  return min;
+};
+
+console.log('min value is:', getMin(arr));
