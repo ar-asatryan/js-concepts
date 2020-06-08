50fe07105d369c0f128cd0d777efd86a47d4a825
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-06-08 12:38:42 +0400

    getArrayByElemsTypes

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-console.log('15');
 
-setTimeout(()=>{
-    console.log(`setTimeOut log`)
-}, 3000);
+// Էս կոդի կտորը ցանկացած հնարավոր ձևով պահելով, ինչ ուզում ես ավելացրու, նենց արա որ this-ը լինի վերևի object-ը
 
-console.log('20');
-console.log('99');
+let obj = {
+    name : "Levon",
+    surname: "Ghukasyan"
+};
+
+function th() {
+
+    setTimeout(()=>{
+        console.log('timer finished>>', this)
+    },1000);
+}
+
+th.call(obj)
 
 
-console.log('>>>>>>>>')
+// Գրել Ֆունկցիա որը կանչելով հետևյալ կերպ կվերադարձնի նոր array որը կունենա element-ներ նշված type-ով
+
+const arr = ["Levon", "Ararat", 15, 16, true, false];
+
+Array.prototype.getArrayByElementTypes = function(type) {
+
+    let newArray = [];
+    for (let i = 0; i <= arr.length; i++) {
+        if ( (typeof arr[i] ) === type) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+
+};
+const newArray =  arr.getArrayByElementTypes("boolean");
+console.log(newArray);
 
 
