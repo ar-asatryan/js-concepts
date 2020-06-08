055bc5008e9eba4c9d25b098addcb8f22bf5a3e8
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-06-08 12:39:45 +0400

    async_tasks.js

--- async_tasks.js
+++ async_tasks.js
@@ -3,15 +3,15 @@ let a = 85;
 let b = 55;
 let c = 165;
 
-if(a<b){
-    console.log("1")
-}
-else if(c<a){
-    console.log("2")
-}
-else{
-    console.log("3")
-};
+// if(a<b){
+//     console.log("1")
+// }
+// else if(c<a){
+//     console.log("2")
+// }
+// else{
+//     console.log("3")
+// };
 
 // Էս նույն կոդը գրել մի տողով, օգատործելով || և && օպերատորները
 
@@ -32,10 +32,32 @@ setTimeout(()=>{
 
 // setInterval-ով ստանալ setTimeout և հակառակը
 
+
 // 4-----------------------------------------------------------------------------
-
-const array = ["Levon", "Ararat", 15, 16, true, false];
-
-const newArray =  array.getArrayByElementTypes("number");
-
 // Գրել Ֆունկցիա որը կանչելով հետևյալ կերպ կվերադարձնի նոր array որը կունենա element-ներ նշված type-ով
+
+const arr = ["Levon", "Ararat", 15, 16, true, false];
+
+Array.prototype.getArrayByElementTypes = function(type) {
+    let newArray = [];
+    for (let i = 0; i <= arr.length; i++) {
+        if ( (typeof arr[i] ) === type) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+};
+const newArray =  arr.getArrayByElementTypes("boolean");
+console.log(newArray)
+
+// 5-----------------------------------------------------------------------------
+// Գրել Ֆունկցիա որը կանչելով հետևյալ կերպ կվերադարձնի նոր array որը կունենա element-ներ նշված type-ով
+
+const a = ["HH","BB","KK","OO","LL"];
+a.insertArrayByNumber(["Levon","Ararat"],2);
+
+//Ստանում ենք
+outputArr = ["HH","BB","Levon","KK","OO","Ararat","LL"];
+
+
+
