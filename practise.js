4f50c92dde8796a316046015252f1f98bf53c91b
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-06-12 23:57:51 +0400

    beautifying view

--- practise.js
+++ practise.js
@@ -21,25 +21,12 @@ const funcExpr = function() {};
 console.log(typeof funcExpr);
 
 
-
-
 setTimeout( function() {
     console.log('console output')
 }, 2000);
 
 
-for(let i = 1; i <= 25; i++) {
 
-    if(i%3 == 0 && i%5 == 0) {
-        console.log('5');
-    } else if (i % 3 == 0) {
-        console.log('3');
-    } else if (i%5 == 0) {
-        console.log("3 and 5")
-    }else {
-        console.log(i)
-    }
-}
 console.log('=================>')
 console.log(typeof {});
 console.log(typeof []);
