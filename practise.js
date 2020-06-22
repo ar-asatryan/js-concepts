6ae044b859d651d851fd66305c522ab21100ca30
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-06-22 23:36:48 +0400

    solved 3 new tasks

--- practise.js
+++ practise.js
@@ -1,7 +1,3 @@
-
-
-//
-
 console.log('execution', 'Operation1')
 console.log('execution', 'Operation2')
 console.log('execution', 'Operation3')
@@ -38,33 +34,84 @@ console.log(Boolean(function(){}))
 
 console.log()
 
+// ToDo: Given a sequence of integers as an array, determine whether it is possible
+//  to obtain a strictly increasing sequence by removing no more than one element from the array.
 
-function display1(cb2,cb3){
-    setTimeout(() => {
-        console.log(1)
+function checkIsAlmostIncreasingSequence(array) {
+    function checkIsArrayIncreased(array,index){
+        for (let i = 0; i < array.length; i++) {
+            if(i === index){
+                continue
+            }
+            else if(array[i] > array[i + 1]){
+                return false
+            }
+        };
+        return true
+    };
+    for(let i = 0; i <array.length; i++){
+        if(checkIsArrayIncreased(array,i)){
+            return true
+        }
+    };
+    return false
+};
+//console.log(checkIsAlmostIncreasingSequence([1, 3, 2]))
 
-        cb2(cb3);
-    }, 4000);
+// ToDo: Find min and max elements in the array and replace them:
+
+Array.prototype.replaceMinAndMaxElements = function () {
+    function getMinOrMaxNumber(array,maxOrMin){
+        for (const element of array) {
+            let minOrMaxNumber = true;
+            for (let index = 0; index < array.length; index++) {
+                if(element<array[index] && maxOrMin){
+                    minOrMaxNumber = false;
+                    break
+                };
+            };
+            if(minOrMaxNumber){
+                return element
+            }
+        }
+    };
+    function replaceTwoElements(array,element1,element2){
+        const index1 = array.indexOf(element1);
+        const index2 = array.indexOf(element2);
+        array[index1] = element2;
+        array[index2] = element1;
+    }
+    replaceTwoElements(this,getMinOrMaxNumber(this,false),getMinOrMaxNumber(this,true));
 };
 
-function display2(cb3){
-    setTimeout(() => {
-        console.log(2)
-        cb3()
-    }, 3000);
+// ToDo: Get the number from the given array which summary of digits is maximum:
+Array.prototype.getElementWithMaximumNumbers = function () {
+    function getElementNumbersList(array) {
+        const numbersQuantityList = [];
+        for (let element of array) {
+            const stringedNumber = String(Math.abs(element));
+            let sum = 0;
+            for (let index = 0; index < stringedNumber.length; index++) {
+                sum += +stringedNumber[index];
+            };
+            numbersQuantityList.push({sum,element})
+        };
+        return numbersQuantityList
+    };
+    function getMaxNumber(array) {
+        for (const element of array) {
+
+            let minOrMaxNumber = true;
+            for (let index = 0; index < array.length; index++) {
+                if (element.sum < array[index].sum) {
+                    minOrMaxNumber = false;
+                    break
+                };
+            };
+            if (minOrMaxNumber) {
+                return element.element
+            }
+        }
+    };
+    return getMaxNumber(getElementNumbersList(this))
 };
-
-function display3(){
-    setTimeout(() => {
-        console.log(3)
-    }, 1000);
-};
-
-function display(cb1,cb2,cb3){
-    cb1(cb2,cb3);
-    // display2();
-    // display3();
-};
-
-display(display1,display2,display3)
-
