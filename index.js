7b170918a8af6b28fbb0f5735249a529948fcd98
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-06-29 15:18:37 +0400

    mySetInterval

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 
 // ToDo:
-const arr = [
-    {
-        cb: ()=>{console.log('element 1:')},
-        time: 2000
-    },{
-        cb: ()=>{console.log('element 2:')},
-        time: 3000
-    },{
-        cb: ()=>{console.log('element 3:')},
-        time: 1000
-    }
-];
 
-function promiseChaining(arr) {
-    const prX = new Promise((res,rej)=>{
-        setTimeout(()=>{
-            arr[0].cb();
-            res()
-        },arr[0].time)
-    });
 
-    prX.then(()=>{
-        arr[1].cb();
-    });
+    // function mySetinterval(cb,time){
+    //     function calling(){
+    //         setTimeout(()=>{
+    //             cb();
+    //             calling();
+    //         }, 1000)
+    //
+    //     }
+    //     calling();
+    // }
+    //
+    //
+    // mySetinterval(()=>{
+    //     console.log(12);
+    // },1000)
 
+
+
+function mySetInterval1(cb,time) {
+    setTimeout(()=>{
+        cb();
+        mySetInterval1(cb,time)
+    },1000)
 }
 
-promiseChaining(arr)
 
 
-
-
+mySetInterval1(()=>{
+    console.log(10)
+},1000)
 
 
 
