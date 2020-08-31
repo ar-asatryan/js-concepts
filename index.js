39769a873609793c79e470d35de37c3bf88d7973
Ararat Asatryan <ar.asatryan94@gmail.com>
2020-08-31 22:16:54 +0400

    getDataURL

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 
 // ToDo:
-function mySetInterval(cb,time) {
-    setTimeout(()=>{
-        cb();
-        mySetInterval(cb,time)
-    },1000)
+
+async function getData(url) {
+
+    return new Promise( (res) => {
+        const timerX = setTimeout( () => {
+            res("get data");
+        }, 3000);
+
+    });
 }
 
-mySetInterval(() => { console.log("logging every second !")}, 1000);
+
+(async function f1() {
+    const arr = ["google.com", "abc", "nodejs.org"];
 
 
+    const arrPromises = [Promise.all(arr.map(elem => getData(elem))),
+        new Promise((res)=>{
+            setTimeout(()=>{
+                res(null)
+            }, 2000)
+        })];
 
+    const data = await Promise.race(arrPromises);
+    console.log(data);
 
-
-
+})();
 
 
 
