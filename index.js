b1779544776fefbb0cbd1284f9b73298b63ab0cf
Ararat Asatryan <ar.asatryan94@gmail.com>
2021-10-23 06:08:39 +0400

    index.js changes

--- index.js
+++ index.js
@@ -1,56 +1,44 @@
-const GAME_CARS_SUITS = ["h", "d", "s", "c"];
-
-const GAME_CARD_SYMBOLS = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
+//
+//implementing JavaScript Array methods...
+const users = [
+  { id: 1, name: "tony", surname: "stark", age: 45, cool: true },
+  { id: 2, name: "steve", surname: "strange", age: 40, cool: true },
+  { id: 3, name: "bruce", surname: "banner", age: 35, cool: false },
+  { id: 4, name: "steve", surname: "rogers", age: 73, cool: false },
+  { id: 5, name: "sia", surname: "yung", age: 33, cool: true },
+  { id: 6, name: "mystic", surname: "man", age: 30, cool: false },
 ];
 
-const getShuffledDeck = () => {
-  return shuffledArray(
-    GAME_CARS_SUITS.map((suit) =>
-      GAME_CARD_SYMBOLS.map((symbol) => `${suit}${symbol}`)
-    ).flat()
-  );
-};
+const numbers = [1, 2, 3, 4, 5];
 
-const shuffledArray = (a) => {
-  const b = [...a];
-  for (let i = b.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [b[i], b[j]] = [b[j], b[i]];
+Array.prototype.myReduce = function(reducer, accumulator) {
+  for(let i = 0; i < this.length; i++) {
+    accumulator = reducer(accumulator, i);
   }
-  return b;
-};
+  return accumulator;
+}
 
-const SOLITAIRE_STOCK_COUNT = 24;
-const SOLITAIRE_COLUMNS_COUNT = 7;
-
-const getSolitaireBoard = () => {
-  const cards = getShuffledDeck();
-  const stock = cards.splice(0, SOLITAIRE_STOCK_COUNT);
-  const columns = [];
-  for (let i = 1; i <= SOLITAIRE_COLUMNS_COUNT; i++) {
-    columns.push(cards.splice(0, i).map((c, j) => (j === i - 1 ? `v${c}` : c)));
+Array.prototype.myMapSimple = function(callback) {
+  const resultArray = [];
+  for(let i = 0; i < this.length; i++) {
+    resultArray.push(callback(this[i]));
   }
-  return {
-    stock,
-    columns,
-  };
-};
+  return resultArray;
+}
 
-const deck = getShuffledDeck();
-console.log("cards deck-------->", deck);
+const usersMap = users.myMapSimple((user) => ({
+  id: user.id,
+  age: user.age,
+  fullname: `${user.name} ${user.surname}`
+}));
 
-const board = getSolitaireBoard();
-console.log("board--------->", board);
+console.log("usersMap:", usersMap)
+
+Array.prototype.myMap = function(callback) {
+  const initArray = [];
+  const mapReducer = (mappedArray, currentValue) =>
+  mappedArray.concat(callback(currentValue))
+  return this.myReduce(mapReducer, initArray);
+}
+
+
